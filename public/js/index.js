853ab57c399bcf8f062108446cbdb9bfc7dc5c9f
KJM <kmaiorini@gmail.com>
2019-02-18 11:35:50 -0500

    Created basic message socket.io events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        to: 'kevin',
+        text: 'Hey, this is Kevin.',
+        createdAt: new Date()
+    })
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
+});
