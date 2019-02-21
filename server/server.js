899f11368d702f14702f8d47328e556d66eca414
KJM <kmaiorini@gmail.com>
2019-02-21 17:47:33 -0500

    Work

--- server/server.js
+++ server/server.js
@@ -14,10 +14,34 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+    
+    // Challenge
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to the chat app!'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New user joined',
+    });
 
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage', newMessage);
-        io.emit('newMessage', {
+        // Sends message to just person who opened socket
+        socket.emit('newMessage', {
+            text: 'New message sent'
+        });
+
+        // Sends message to everyone
+        // io.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        // Sends message to everyone except who opened socket
+        socket.broadcast.emit('newMessage', {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
